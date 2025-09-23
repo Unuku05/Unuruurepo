3d7b2dae1b1f928f3816885f02743d96bc648331
Unuku05 <143775909+Unuku05@users.noreply.github.com>
2025-09-23 14:09:11 +0800

    Initial commit

---
+++ about/page.tsx
@@ -0,0 +1,25 @@
+import Layout from "../components/layout";
+import styles from "./page.module.css";
+import Link from "next/link";
+export default function About() {
+  return (
+    <>
+      <Layout>
+      <main className="p-6">
+        <h2 className="text-2xl font-bold mb-4">Миний тухай</h2>
+        <p className="mb-6">Next.js ашиглаж байна.</p>
+
+        <section className="flex flex-col items-center justify-center text-center">
+          <h1 className={styles.title}>Unuruugiin namtar</h1>
+          <p className={styles.description}>
+           Mandakh surguuliin oyutan
+          </p>
+          <p> Bi 2005 oni 9n sariin 21nd tursun </p>
+          <p>Morin khuur togloh durtai</p>
+        </section>
+
+      </main>
+      </Layout>
+    </>
+  );
+}
