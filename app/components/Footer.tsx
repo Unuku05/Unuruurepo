98777712f69138eaf87f3a7b21fe1de232ef04b9
Unuku05 <143775909+Unuku05@users.noreply.github.com>
2026-04-06 13:48:37 +0800

    ok

--- app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return(
-        <footer className="items-center justify-items-center bg-gray-600 text-white p-4">
-            <h1>Unuruugiin LAB2</h1>
-        </footer>
-    )   
-}   
